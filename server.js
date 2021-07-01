d37a14507e018199eded4d4ee0992d089fffef6d
Jon <51219855+JonSP6@users.noreply.github.com>
2021-06-30 21:10:35 -0400

    chore: login route started

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
 
 const posts = [
   {
@@ -16,4 +19,11 @@ app.get("/posts", (req, res) => {
   res.json(posts);
 });
 
+//*Authenticate requests using JWT
+app.post("/login", (req, res) => {
+  //1. Authenticate User (Enter bcrypt authentication here)
+  //For right now we'll skip the Authentication and focus on JWT functionality
+  //2. Create JWT -> Once User has passed the authentication we then want to authenticate and serialize this User with JSON Web Tokens (JWT)
+  const username = req.body.username;
+});
 app.listen(3000, () => console.log("App listening at PORT 3000"));
