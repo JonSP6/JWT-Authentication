6d6a9dc12e5ab5be65349a44418fa00ae4c0b627
Jon <51219855+JonSP6@users.noreply.github.com>
2021-07-01 21:14:15 -0400

    chore: Secret Key in dotenv. accesstoken created with jwt.sign().

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
@@ -25,5 +27,13 @@ app.post("/login", (req, res) => {
   //For right now we'll skip the Authentication and focus on JWT functionality
   //2. Create JWT -> Once User has passed the authentication we then want to authenticate and serialize this User with JSON Web Tokens (JWT)
   const username = req.body.username;
+  const user = { name: username };
+
+  //.sign() takes our payload (what we want to serialize) in this case a user Object.
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+  // next we want the Server to return back to the Client the accessToken that was just created. Note: Remember the accessToken contains the User information saved inside of it.
+  res.json({ accessToken: accessToken });
 });
+
 app.listen(3000, () => console.log("App listening at PORT 3000"));
