a89849ca93fc3c7f78608b3f9ab6cb425729a723
Jon <51219855+JonSP6@users.noreply.github.com>
2021-07-02 20:42:54 -0400

    chore: removed comments

--- server.js
+++ server.js
@@ -23,16 +23,11 @@ app.get("/posts", authenticateToken, (req, res) => {
 
 //*Authenticate requests using JWT
 app.post("/login", (req, res) => {
-  //1. Authenticate User (Enter bcrypt authentication here)
-  //For right now we'll skip the Authentication and focus on JWT functionality
-  //2. Create JWT -> Once User has passed the authentication we then want to authenticate and serialize this User with JSON Web Tokens (JWT)
   const username = req.body.username;
   const user = { name: username };
 
-  //.sign() takes our payload (what we want to serialize) in this case a user Object.
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 
-  // next we want the Server to return back to the Client the accessToken that was just created. Note: Remember the accessToken contains the User information saved inside of it.
   res.json({ accessToken: accessToken });
 });
 
