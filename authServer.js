8c849a9e78f2d1fb1c1beb84682b8a36ea449f2d
Jon <51219855+JonSP6@users.noreply.github.com>
2021-07-05 20:54:59 -0400

    chore:

--- authServer.js
+++ authServer.js
@@ -6,19 +6,13 @@ const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
-const posts = [
-  {
-    username: "Kyle",
-    title: "Post 1",
-  },
-  {
-    username: "Jim",
-    title: "Post 2",
-  },
-];
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
 
-app.get("/posts", authenticateToken, (req, res) => {
-  res.json(posts.filter((post) => post.username === req.user.name));
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+  res.json({ accessToken: accessToken });
 });
 
 function authenticateToken(req, res, next) {
