07beb7dddd82c1e6bb339578b78dc9b73dfd0f4e
Jon <51219855+JonSP6@users.noreply.github.com>
2021-07-06 20:56:29 -0400

    chore: /token route created to check refreshToken and generate accessToken

--- authServer.js
+++ authServer.js
@@ -6,13 +6,26 @@ const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
+let refreshTokens = []; //Store refreshTokens in DB but for now use this to test.
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    return res.json({ accessToken: accessToken });
+  });
+});
+
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
 
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-
+  refreshTokens.push(refreshToken);
   res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
